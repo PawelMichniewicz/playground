fc7ad0d434f9a9afe55f0c88dded29607c8790fe
Paweł Michniewicz <48495023+PawelMichniewicz@users.noreply.github.com>
2022-01-25 19:42:09 +0100

    Added Model for SensorConfig. Config Parser started

---
+++ training/Models/SensorConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace Training.Models
+{
+    class SensorConfig
+    {
+        public int ID { get; set; }
+
+        public string Type { get; private set; }
+
+        public int MinValue { get; private set; }
+
+        public int MaxValue { get; private set; }
+
+        public string Encoder { get; private set; }
+
+        public int Frequency { get; private set; }
+    }
+}
---
+++ training/SensorParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class SensorParser
+    {
+        public SensorParser(string path)
+        {
+            this.Path = path;
+        }
+
+        public string Path { get; private set; }
+        
+        public bool LoadConfig()
+        {
+            // if file not exists throw
+            if (string.IsNullOrWhiteSpace(Path))
+            {
+                throw new ArgumentNullException($"Cannot parse config file: {Path}");
+            }
+            // else: open file under path
+
+            return true;
+        }
+
+        public void Parse()
+        {
+        }
+    }
+}
