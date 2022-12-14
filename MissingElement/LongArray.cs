47ebfebcceed1f54f7c6fa33ba5ffceda36e860c
PawelMichniewicz <48495023+PawelMichniewicz@users.noreply.github.com>
2022-12-14 22:57:58 +0100

    added project to solve task

---
+++ MissingElement/LongArray.cs
@@ -0,0 +1,44 @@
+﻿namespace MissingElement
+{
+    internal class LongArray//<T> where T : IComparable
+    {
+        private readonly int size;
+        private readonly int min;
+        private readonly int max;
+        private int[] collection;
+
+        public LongArray(int maxValue)
+        {
+            min = 1;
+            max = maxValue;
+            this.size = maxValue - 1;
+
+            Random rng = new(DateTime.Now.Microsecond);
+            collection = Enumerable.Range(min, max).OrderBy(x => rng.Next()).Take(size).ToArray();
+        }
+
+        public void Foo()
+        {
+            int i = 0;
+            int t = 1;
+            do
+            {
+                print();
+                t = collection[i];
+                collection[i] = i + 1;
+                i = t - 1;
+            } while (true);
+        }
+
+        private void print()
+        {
+            Console.Write("{");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"{collection[i]},");
+            }
+            Console.WriteLine("}");
+
+        }
+    }
+}
---
+++ MissingElement/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MissingElement
+{
+    // array with size of N
+    // shuffled non repeating values with max at N+1
+    // one value is missing. find which one
+    // EXAMPLE:
+    // array size 10, idexes 0 - 9
+    // values range: 1 - 10
+    // array: {9, 1, 8, 10, 3, 6, 5, 7, 4} - 2 is missing
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LongArray temp = new LongArray(10);
+            temp.Foo();
+        }
+    }
+}
